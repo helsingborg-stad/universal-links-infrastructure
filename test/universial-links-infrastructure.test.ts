80b8cc06acfda7594b07e21dae66349c56b60ef5
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-03-07 13:20:26 +0100

    Initial commit

---
+++ test/universial-links-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UniversialLinksInfrastructure from '../lib/universial-links-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/universial-links-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UniversialLinksInfrastructure.UniversialLinksInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
